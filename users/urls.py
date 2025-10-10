1340e9c8bdc65d80a306dc436c01e654e759fd47
Мальков Олег <karielka@yandex.ru>
2025-10-11 00:01:17 +0300

    add main styles and pages

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,8 @@ app_name = "users"
 
 urlpatterns = [
 
+    path("login/",     views.LoginView.as_view(), name="login"),
+    path("profile/",   views.ProfileView.as_view(), name="profile"),
+    path("register/",  views.RegisterView.as_view(), name="register"),
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect, render
+from django.http import JsonResponse, HttpResponseBadRequest
+from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib import messages
+from django.utils import timezone
+from django.urls import reverse
 
-# Create your views here.
+
+class LoginView(TemplateView):
+    template_name = "users/login.html"
+
+
+class RegisterView(TemplateView):
+    template_name = "users/register.html"
+
+
+class ProfileView(TemplateView):
+    template_name = "users/profile.html"
